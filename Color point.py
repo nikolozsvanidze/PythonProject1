464072f4b07946de190141303a6cf6d4755b516f
mac <nsvanidze.ieu2023@student.ie.edu>
2025-03-19 16:40:56 +0100

    Assed better __init__

--- Color point.py
+++ Color point.py
@@ -2,8 +2,19 @@ import random
 
 from point import Point
 
+class PointException(Exception):
+    pass
+
 class ColorPoint(Point):
     def __init__(self, x, y, color):
+       # raise an expection if we try to have not a number
+        if not isinstance(x,(int, float)):
+            raise TypeError("x must be a number")
+        if not isinstance(x,(int, float)):
+            raise TypeError("y must be a number")
+        super().__init__(x, y)
+        self.color = color
+
         self.x = x
         self.y = y
         self.color = color
@@ -12,13 +23,22 @@ class ColorPoint(Point):
         return f"<{self.color}: {self.x}, {self.y}>"
 
 p = ColorPoint(1, 2, "red")
+print(p.distance_orig())
 print(p)
-color = ["red","green", "blue", "yello", "black",]
-color_points = []
-for i in range(10):
-    color_points.append(
-        ColorPoint(random.randint(-10, 10),
-                   random.randint(-10, 10),
-                   random.choice(colors)))
-    print(color_points)
-    color_points
+
+
+
+
+
+
+# print(p)
+# color = ["red","green", "blue", "yello", "black",]
+# color_points = []
+# for i in range(10):
+#     color_points.append(
+#         ColorPoint(random.randint(-10, 10),
+#                    random.randint(-10, 10),
+#                    random.choice(colors)))
+#     print(color_points)
+#
+
--- point.py
+++ point.py
@@ -26,26 +26,27 @@ class Point:
         other_distance = other.distance_orig()
         return my_distance == other_distance
 if __name__ == "__main__":
-#now we need to instentiate it
-p = Point(1, 2)  # p is an intance of 1 and 2
-p2 = Point(2, 3)
-p4 = Point(4.4, -55)
-print(f"p.x={p.x} and p.y={p.y}")
-print(f"p4.x={p4.x} and p4.y={p4.y}")
-p.x = 20
-print(f"p.x={p.x} and p.y={p.y}")
-print(p)
-#create a list of 5 random points
-points = []
-for i in range(5):
-    points.append(Point(random.randint(-10, 10), # x value
-                        random.randint(-10, 10))) # y value
-print("I got these 5 random points:")
-print(points)
-p= Point(3, 4)
-print(p.distance_orig()) #expect 5 answers
-p2 = Point(1,1)
-print(f"I am comparing p > p2{p>p2}")
-print("the sorted list of points is:")
-points.sort()
-print(points)
+
+     #now we need to instentiate it
+    p = Point(1, 2)  # p is an intance of 1 and 2
+    p2 = Point(2, 3)
+    p4 = Point(4.4, -55)
+    print(f"p.x={p.x} and p.y={p.y}")
+    print(f"p4.x={p4.x} and p4.y={p4.y}")
+    p.x = 20
+    print(f"p.x={p.x} and p.y={p.y}")
+    print(p)
+    #create a list of 5 random points
+    points = []
+    for i in range(5):
+        points.append(Point(random.randint(-10, 10), # x value
+                            random.randint(-10, 10))) # y value
+    print("I got these 5 random points:")
+    print(points)
+    p= Point(3, 4)
+    print(p.distance_orig()) #expect 5 answers
+    p2 = Point(1,1)
+    print(f"I am comparing p > p2{p>p2}")
+    print("the sorted list of points is:")
+    points.sort()
+    print(points)
